1f69826c9133ec8c0bd9254addab39fe7eb00e74
Connor Linfoot <connorlinfoot@me.com>
2014-08-27 14:26:55 +0100

    Added an option to remember the players names (After reload or restart players will still have their display names)

--- src/com/connorlinfoot/displaynamechanger/Main.java
+++ src/com/connorlinfoot/displaynamechanger/Main.java
@@ -4,8 +4,11 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Server;
 import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Map;
 
 
 public class Main extends JavaPlugin {
@@ -22,11 +25,29 @@ public class Main extends JavaPlugin {
         console.sendMessage(ChatColor.GREEN + "=========== VERSION: 2.0 ===========");
         console.sendMessage(ChatColor.GREEN + "======== BY CONNOR LINFOOT! ========");
 
+        if( Main.getInstance().getConfig().getBoolean("Remember Names") && instance.getConfig().getConfigurationSection("Players") != null ) {
+            ConfigurationSection cs = instance.getConfig().getConfigurationSection("Players");
+
+            for (String player : cs.getKeys(false)) {
+                if (instance.getConfig().get("Players." + player + ".Name") != null) {
+                    Command.names.put(player,instance.getConfig().getString("Players." + player + ".Name"));
+                }
+            }
+        }
+
         Bukkit.getPluginManager().registerEvents(new Listener(),this);
         Bukkit.getPluginCommand("cdn").setExecutor(new Command());
     }
 
     public void onDisable() {
+        if( Main.getInstance().getConfig().getBoolean("Remember Names") ) {
+            for (Map.Entry<String, String> entry : Command.names.entrySet()) {
+                String key = entry.getKey();
+                Object value = entry.getValue();
+                getConfig().set("Players." + key + ".Name", value);
+            }
+            saveConfig();
+        }
         getLogger().info(getDescription().getName() + " has been disabled!");
     }
 
